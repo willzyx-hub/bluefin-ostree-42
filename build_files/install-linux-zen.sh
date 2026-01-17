0f626713a7e589cc1b3ae0b2850be6ecf0561005
William Liu <williamliu636@gmail.com>
2026-01-17 18:26:16 +0700

    Try to fix initramfs not being generated

--- build_files/install-linux-zen.sh
+++ build_files/install-linux-zen.sh
@@ -26,6 +26,17 @@ echo "Kernel Override complete"
 # Fix /var/run invariant (bootc requirement)
 rm -rf /var/run
 ln -s ../run /var/run
+echo "Fix Bootc requirement completed"
+
+# Fix initramfs not being created
+# Specify kernel version
+KERNEL_VERSION="6.18.5_zen+-15" # Set variables
+
+# Generate the initramfs for this kernel
+/usr/bin/dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible -v --add ostree -f "/lib/modules/$KERNEL_VERSION/initramfs.img"
+
+# Make sure permissions are correct
+chmod 0600 "/lib/modules/$KERNEL_VERSION/initramfs.img"
 
 # Restore kernel version lock
 #dnf5 versionlock add kernel-core-${KERNEL_VERSION} kernel-modules-${KERNEL_VERSION} kernel-modules-extra-${KERNEL_VERSION}
